2903e5d2b125c3975f9dbead92762d726d8f6bc0
arthur_amorim_ <arthur.silva@korp.com.br>
2023-08-03 10:23:12 -0300

    Serilog and comments removed

--- SentryBackgroundJobShowcase/Program.cs
+++ SentryBackgroundJobShowcase/Program.cs
@@ -7,24 +7,15 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
 
-builder.Host.UseSerilog((_, loggerConfiguration) =>
-{ 
-    loggerConfiguration.ReadFrom.Configuration(configuration);
-});
-
-builder.WebHost.UseSentry(
-    o =>
-{
-    o.AddEventProcessor(new MyEventProcessor());
-}
-    );
+builder.WebHost.UseSentry();
 
 builder.Services
     .AddHangfireServer()
@@ -35,16 +26,13 @@ builder.Services
         .UseSQLiteStorage(builder.Configuration.GetConnectionString("DefaultConnection")))
     .AddAuthorization()
     .AddHostedService<AddBackgroundJobHostedService>()
-    //.AddTransient<ISentryEventProcessor, MyEventProcessor>()
-    ;
+    .AddTransient<ISentryEventProcessor, MyEventProcessor>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
